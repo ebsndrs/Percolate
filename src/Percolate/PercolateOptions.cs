6b5ae04ac84e018cee61bb5dd35492e2cf251bce
Edward Sanders <esas@tobii.com>
2020-02-20 14:49:49 -0500

    Added a few properties to the options

--- src/Percolate/PercolateOptions.cs
+++ src/Percolate/PercolateOptions.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Percolate
+﻿namespace Percolate
 {
     public class PercolateOptions
     {
+        public bool EnablePercolateGlobally { get; set; }
+
+        public bool EnablePagingGlobally { get; set; }
+
+        public bool EnableSortingGlobally { get; set; }
+
+        public bool EnableFilteringGlobally { get; set; }
+
+        public bool EnableSelectingGlobally { get; set; }
+
+        public bool EnableExpandingGlobally { get; set; }
+
+        public int DefaultPageSize { get; set; }
+
+        public int MaxPageSize { get; set; }
     }
 }
