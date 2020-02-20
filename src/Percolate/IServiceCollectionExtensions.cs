7922a0bb47be45cec3c91765f103368fb996cd3c
Edward Sanders <esas@tobii.com>
2020-02-20 14:39:54 -0500

    Added skeletons for services registration and attributes/filters

---
+++ src/Percolate/IServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Percolate
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPercolate(this IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IServiceCollection AddPercolate(this IServiceCollection services, Action<PercolateOptions> options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IServiceCollection BuildPercolateService(this IServiceCollection services, Action<PercolateOptions> options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Percolate/PercolateActionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Percolate
+{
+    public class PercolateActionFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
