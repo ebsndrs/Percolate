90d331b248376279e3343445b19197184e2d9b6e
Edward Sanders <esas@tobii.com>
2020-02-20 14:46:43 -0500

    Added attribute helper to get custom attributes from controllers and controller actions

---
+++ src/Percolate/Attributes/AttributeHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Percolate.Attributes
+{
+    internal static class AttributeHelper
+    {
+        internal static TAttribute GetAttributeFromAction<TAttribute>(ActionDescriptor actionDescriptor) where TAttribute : Attribute
+        {
+            TAttribute attribute = null;
+
+            if (actionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+            {
+                attribute = controllerActionDescriptor
+                    .MethodInfo
+                    .GetCustomAttributes(typeof(TAttribute))
+                    .SingleOrDefault() as TAttribute;
+            }
+
+            return attribute;
+        }
+
+        internal static TAttribute GetAttributeFromController<TAttribute>(ActionDescriptor actionDescriptor) where TAttribute : Attribute
+        {
+            TAttribute attribute = null;
+
+            if (actionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+            {
+                attribute = controllerActionDescriptor
+                    .ControllerTypeInfo
+                    .GetCustomAttributes(typeof(TAttribute))
+                    .SingleOrDefault() as TAttribute;
+            }
+
+            return attribute;
+        }
+    }
+}
