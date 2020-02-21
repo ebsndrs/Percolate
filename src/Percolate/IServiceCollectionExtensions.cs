ef2edb90e33830ac28bd99be4f4151649a892a15
Edward Sanders <esas@tobii.com>
2020-02-20 21:38:56 -0500

    Added methods to register Percolate as a service

--- src/Percolate/IServiceCollectionExtensions.cs
+++ src/Percolate/IServiceCollectionExtensions.cs
@@ -7,17 +7,20 @@ namespace Percolate
     {
         public static IServiceCollection AddPercolate(this IServiceCollection services)
         {
-            throw new NotImplementedException();
+            static void options(PercolateOptions options) => new PercolateOptions();
+            return services.BuildPercolateService(options);
         }
 
         public static IServiceCollection AddPercolate(this IServiceCollection services, Action<PercolateOptions> options)
         {
-            throw new NotImplementedException();
+            return services.BuildPercolateService(options);
         }
 
-        public static IServiceCollection BuildPercolateService(this IServiceCollection services, Action<PercolateOptions> options)
+        private static IServiceCollection BuildPercolateService(this IServiceCollection services, Action<PercolateOptions> options)
         {
-            throw new NotImplementedException();
+            services.AddMvcCore(options => options.Filters.Add<PercolateActionFilter>());
+
+            return services;
         }
     }
 }
