58a9c02da2f4b08c2b1d25d21948e06a75e2cd31
Edward Sanders <esas@tobii.com>
2020-02-21 10:56:48 -0500

    Refactored the FilterParser to use Regex for more certain matches

--- src/Percolate/Parsers/FilterParser.cs
+++ src/Percolate/Parsers/FilterParser.cs
@@ -1,35 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.Extensions.Primitives;
+﻿using Microsoft.AspNetCore.Http;
 using Percolate.Exceptions;
 using Percolate.Models.Filtering;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Percolate.Parsers
 {
     internal static class FilterParser
     {
-        private static readonly Dictionary<FilterOperator, string> operatorDictionary = new Dictionary<FilterOperator, string>()
+        private static readonly Dictionary<FilterOperator, string> filterOperators = new Dictionary<FilterOperator, string>()
         {
             { FilterOperator.Equals, "==" },
             { FilterOperator.DoesNotEqual, "!=" },
-            { FilterOperator.GreaterThanOrEqual, ">=" },
-            { FilterOperator.LessThanOrEqual, "<=" },
             { FilterOperator.GreaterThan, ">" },
             { FilterOperator.LessThan, "<" },
+            { FilterOperator.GreaterThanOrEqual, ">=" },
+            { FilterOperator.LessThanOrEqual, "<=" }
         };
 
-        internal static FilterModel ParseFilterParameter(ActionExecutedContext actionExecutedContext)
+        internal static FilterModel ParseFilterParameter(IQueryCollection queryCollection)
         {
-            var sortModel = new FilterModel();
+            var filterModel = new FilterModel();
 
-            if (actionExecutedContext.HttpContext.Request.Query.ContainsKey("filter"))
+            if (queryCollection.ContainsKey("filter"))
             {
-                var queryStrings = actionExecutedContext.HttpContext.Request.Query["filter"].ToString().Split(',');
-                sortModel.Nodes = ParseFilterParameterNodes(queryStrings);
+                var queryStrings = queryCollection["filter"].ToString().Split(',');
+                filterModel.Nodes = ParseFilterParameterNodes(queryStrings);
             }
 
-            return sortModel;
+            return filterModel;
         }
 
         private static IEnumerable<FilterNode> ParseFilterParameterNodes(string[] queryStrings)
@@ -39,32 +39,26 @@ namespace Percolate.Parsers
 
         private static FilterNode ParseFilterParameterNode(string queryString)
         {
-            bool operatorSearchFailed = true;
-            FilterOperator? filterOperator = null;
-            string[] values = new string[3];
-
-            foreach (var op in operatorDictionary)
+            //loop through the possible operators to try and find a match
+            foreach (var op in filterOperators)
             {
-                if (queryString.Contains(op.Value))
+                //insert the operator string into our regex pattern to check if it matches
+                //the regex only matches if exactly one of the filter operators is present in the string
+                //this is more certain than string.Contains() because it ensures an exact character match
+                if (Regex.IsMatch(queryString, $@"^\w+({op.Value}){{1}}[^=!><][\s\S]+$"))
                 {
-                    values = queryString.Split(op.Value);
-                    filterOperator = op.Key;
-                    operatorSearchFailed = false;
-                    break;
+                    var values = queryString.Split(op.Value);
+                    return new FilterNode
+                    {
+                        PropertyName = values[0],
+                        Operator = op.Key,
+                        FilterValue = values[1]
+                    };
                 }
             }
 
-            if (operatorSearchFailed)
-            {
-                throw new ParameterParsingException();
-            }
-
-            return new FilterNode
-            {
-                PropertyName = values[0],
-                Operator = filterOperator.Value,
-                FilterValue = values[1]
-            };
+            //if we reached the end of the loop, no match was found
+            throw new ParameterParsingException();
         }
     }
 }
