18bd9a530c608a03cb147592f4a5fcddc8d8ef8e
Edward Sanders <ebsndrs@gmail.com>
2020-04-08 14:11:21 -0400

    Removed old NetCore project

--- src/Percolate.NetCore/Attributes/DisablePercolateAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Percolate.Attributes
-{
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-    public sealed class DisablePercolateAttribute : Attribute
-    {
-    }
-}
